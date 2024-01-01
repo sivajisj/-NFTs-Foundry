663cf0d9fa94733db10939f6a3bd0a345872ac6a
javas <sivajigsivajig703@gmail.com>
2024-01-01 08:55:16 +0000

    delploy scripts

--- Foundry-NFT-23/script/Interactions.s.sol
+++ Foundry-NFT-23/script/Interactions.s.sol
@@ -12,10 +12,11 @@ contract MintBasicNft is Script {
     string public constant PUG_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-            "BasicNft", 
-            block.chainId
-            );
+        // address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+        //     "BasicNft", 
+        //     block.chainid
+        //     );
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
 
             mintNftOnContract(mostRecentlyDeployed);
     }
